31b2795ea1f42faaa5974d4abbf46cfdc7fa71c3
tanwinder <tanwinder.ui@gmail.com>
2021-07-04 18:17:12 -0500

    fix build error

--- search/src/index.js
+++ search/src/index.js
@@ -2,7 +2,7 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import {render} from 'react-dom'
 
-import App from './components/app'
+import App from './components/App'
 
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
