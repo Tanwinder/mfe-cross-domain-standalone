e64ac37e623878fd8f44756a0433c1f50d92e6c0
Tanwinder <tanwinder.ui@gmail.com>
2021-07-30 08:43:57 -0400

    Update controllers.js

--- home/src/mocks/controllers.js
+++ home/src/mocks/controllers.js
@@ -1,6 +1,5 @@
 import json from './data/signin.json';
 export const signinFun = (req, res, ctx) => {
   const { username } = req.body;
-  debugger;
   return res(ctx.status(200), ctx.json(json));
 };
